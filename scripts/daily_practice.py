673f240046c525f157673e22708dd2daaa2bbcc3
AchalBuilds <achalagarwal777@gmail.com>
2026-04-27 12:16:59 +0530

    Update daily_practice.py

--- scripts/daily_practice.py
+++ scripts/daily_practice.py
@@ -1,1 +1,10 @@
 
+name = input("What is your name?:")
+
+if name == "Achal":
+  print("Access Granted. Welcome, Developer.")
+else:
+  print("Identity unverified. Initializing standard visitor profile.")
+
+
+  
