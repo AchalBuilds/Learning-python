2519dc93f2affbc72b7e266ec9dc7ef610e9dfc0
AchalBuilds <achalagarwal777@gmail.com>
2026-04-27 12:54:11 +0530

    Update daily_practice.py

--- scripts/daily_practice.py
+++ scripts/daily_practice.py
@@ -2,7 +2,11 @@
 name = input("What is your name?:")
 
 if name == "Achal":
-  print("Access Granted. Welcome, Developer.")
+  code = int(input("Enter the secret code:"))
+  if code == 1234:
+      print("Access Granted. Welcome, Developer.")
+  else: 
+      print("Wrong Code. Access Denied.")
 else:
   print("Identity unverified. Initializing standard visitor profile.")
 
