784d4b331f90e8c9f35ea4c45026ee502b6df253
AchalBuilds <achalagarwal777@gmail.com>
2026-05-06 08:34:29 +0000

    More currencies added

--- scripts/currency_converter_v2.py
+++ scripts/currency_converter_v2.py
@@ -1,10 +1,27 @@
 
 rates = {
     "USD" : 1.0,
-    "INR" : 94.84,
+    "INR" : 94.9,
     "EUR" : 0.85,
-    "JPY" : 156.40,
-    "RUB" : 75.0
+    "JPY" : 156.1,
+    "RUB" : 75.0,
+    "CNY" : 6.8,
+    "CAD" : 1.35,
+    "MXN" : 17.4,
+    "BRL" : 4.95,
+    "COP" : 3710,
+    "ARS" : 1390,
+    "CLP" : 910,
+    "GBP" : 0.74,
+    "CHF" : 0.78,
+    "TRY" : 45.24,
+    "PKR" : 278,
+    "AUD" : 1.38,
+    "SGD" : 1.27,
+    "NZD" : 1.67,
+    "PHP" : 61.3,
+    "KRW" : 1453,
+    "IDR" : 17390
          }
 
 print("------Global Exchange Converter------")
@@ -15,8 +32,8 @@ while amount <0 :
     print("Error: Transaction Cancelled. Amount cannot be negative.")
     amount = float(input("Please enter a valid amount: "))
 
-from_cur = input("From (USD/INR/EUR/JPY/RUB): ").upper()
-to_cur = input("To (USD/INR/EUR/JPY/RUB): ").upper()
+from_cur = input("From (a valid currency abbrevation): ").upper()
+to_cur = input("To (a valid currency abbreviation): ").upper()
 
 print()
 
@@ -28,4 +45,4 @@ if from_cur in rates and to_cur in rates:
     print(f"Exchange rate: {exchange_rate}") 
     print(f"Result: {result} {to_cur}")
 else:
-    print("Invalid currency entered! Please check the codes.")
+    print("Invalid currency entered!")
